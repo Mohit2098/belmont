a6598ffd828a618fd8aae14959bf5ea66f8b52d3
michael <michael.mustfa@ithands.net>
2023-01-31 15:30:28 +0530

    add: css files created

--- wp-content/themes/belmont_theme/_includes/functions-enqueue.php
+++ wp-content/themes/belmont_theme/_includes/functions-enqueue.php
@@ -15,6 +15,8 @@
 // 1. Enqueue everything
 function wp_nq_scripts() {
   wp_enqueue_style( 'style', get_stylesheet_uri(),null,1.0);
+  wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri().'/_css/custom.css',null,1.0);
+  wp_enqueue_style( 'layout-style', get_stylesheet_directory_uri().'/_css/layout.css',null,1.0);
   wp_enqueue_script ('wp-script', get_stylesheet_directory_uri() . '/_js/js.js', array('jquery'), 1.0, true);
 }
 add_action('wp_enqueue_scripts', 'wp_nq_scripts');
