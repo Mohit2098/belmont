1d2ce2f25d9debb23954e4f25a6f78780421c7e5
melvin.mohit <melvin.mohit@rubicotech.in>
2023-01-31 13:08:07 +0530

    added: div and ancher tag for header and footer logo

--- wp-content/themes/belmont_theme/footer.php
+++ wp-content/themes/belmont_theme/footer.php
@@ -1,14 +1,15 @@
     <footer class="global-footer">
-    <?php $site_logo = get_field('site_logo', 'option'); ?>
-        <?php if(!empty($site_logo)):?>
-         <img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>"/>
-      <?php endif;?>
+        <div class="footer-logo">
+            <?php $site_logo = get_field('site_logo', 'option'); ?>
+            <?php if(!empty($site_logo)):?>
+            <a href="<?php echo home_url( '/' ); ?>" rel="home" ><img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>"/></a>
+            <?php endif;?>
+        </div>
         <nav aria-label="<?php _e( 'Footer Navigation', 'wpbp-theme' ); ?>">
             <?php wp_nav_menu( array( 'container_class' => 'menu-footer', 'theme_location' => 'footer_menu' ) ); ?>
         </nav>
         <?php $copyright_text = get_field('copyright_text', 'option');?>
-        <?php echo $copyright_text;?>
-        <?php get_search_form(); ?>
+        <div class="footer-copyright"><?php echo $copyright_text;?></div>
     </footer>
     <?php wp_footer(); ?>
     </body>
--- wp-content/themes/belmont_theme/header.php
+++ wp-content/themes/belmont_theme/header.php
@@ -9,16 +9,16 @@
   	<?php wp_body_open(); ?>
   	<a class="screen-reader-text" href="#content"><?php _e( 'Skip to Main Content', 'wpbp-theme' ); ?></a>
   	<header class="global-header">
-  		<a href="<?php echo home_url( '/' ); ?>" rel="home" ><?php bloginfo( 'name' ); ?></a>
-		  <?php $site_logo = get_field('site_logo', 'option'); ?>
-        <?php if(!empty($site_logo)):?>
-         <img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>"/>
-      <?php endif;?>
-		  <nav id="access" aria-label="<?php _e( 'Top Bar Navigation', 'wpbp-theme' ); ?>" >
-  			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'top_bar_menu' ) ); ?>
+		<div class="header-logo">
+			<?php $site_logo = get_field('site_logo', 'option'); ?>
+			<?php if(!empty($site_logo)):?>
+				<a href="<?php echo home_url( '/' ); ?>" rel="home" ><img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>"/></a>
+				<?php endif;?>
+			</div>
+		<nav id="access" aria-label="<?php _e( 'Top Bar Navigation', 'wpbp-theme' ); ?>" >
+  		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'top_bar_menu' ) ); ?>
   		</nav>
   		<nav id="access" aria-label="<?php _e( 'Primary Navigation', 'wpbp-theme' ); ?>" >
   			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary_menu' ) ); ?>
   		</nav>
-  		<?php include_once('header-searchform.php'); ?>
   	</header>
