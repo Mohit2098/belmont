6bccb3492532cc4b47ddbfa35dc2637c759f440c
melvin.mohit <melvin.mohit@rubicotech.in>
2023-02-01 09:44:31 +0530

    created: button link field using array in banner section

--- wp-content/themes/belmont_theme/_layouts/banner_section.php
+++ wp-content/themes/belmont_theme/_layouts/banner_section.php
@@ -1,29 +1,37 @@
-<section class="banner-section">
-    <div class=" container">
-        <?php if( have_rows('slides') ):?>
-         <div class="banner-slider">
-        <?php while( have_rows('slides') ): the_row();
-                  $content_heading = get_sub_field('heading');
-                  $content_description = get_sub_field('sub_heading');
-                  $content_image = get_sub_field('image');
-                  $button_link = get_sub_field('button_link'); 
-               ?>
-               <div>
-                     <?php
-                     if(!empty($content_heading)){
-                        echo "<h1>".$content_heading."</h1>";
+   <section class="banner-section">
+      <div class=" container">
+         <?php if( have_rows('slides') ):?>
+            <div class="banner-slider">
+               <?php while( have_rows('slides') ): the_row();
+                     $banner_heading = get_sub_field('heading');
+                     $banner_sub_heading = get_sub_field('sub_heading');
+                     $banner_button_link = get_sub_field('button_link'); 
+                     $banner_image = get_sub_field('image');
+                  ?>
+                  <div>
+                     <?php if(!empty($banner_heading)){
+                        echo "<h1>".$banner_heading."</h1>";
                      }?>
-                  <?php if(!empty($content_image)) {?>
-                     <a href="#"><img src="<?php echo esc_url($content_image['url']); ?>" alt="<?php echo esc_attr($content_image['alt']); ?>"/></a>
+
+                     <?php if(!empty($banner_sub_heading)){ 
+                        echo "<div>".$banner_sub_heading."</div>";
+                     }?>
+
+                     <?php if(!empty($banner_button_link)){ ?>
+                     <?php $button_url = $banner_button_link['url'];
+                     $button_title = $banner_button_link['title'];
+                     $button_target = $banner_button_link['target']? $banner_button_link['target']:'_self';?>
+                     <a class="button" href="<?php echo $button_url?>" target = "<?php echo $button_target?>">
+                     <?php echo $button_title;?></a>
+                     <?php } ?>
+
+                     <?php if(!empty($banner_image)) {?>
+                        <a href="#"><img src="<?php echo esc_url($banner_image['url']); ?>" 
+                        alt="<?php echo esc_attr($banner_image['alt']); ?>"/></a>
                      <?php }?>
-                <?php
-               if(!empty($content_description)){ 
-                  echo "<div>".$content_description."</div>";
-               }
-               ?>
-             </div>
-        <?php endwhile; ?>
-        </div>
-        <?php endif; ?>
-    </div>
-</section>
+                  </div>
+               <?php endwhile;?>
+            </div>
+         <?php endif;?>
+      </div>
+   </section>
