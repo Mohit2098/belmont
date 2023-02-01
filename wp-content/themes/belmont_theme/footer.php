11832e16df51559aa6b19747a9c7e98ae2077323
melvin.mohit <melvin.mohit@rubicotech.in>
2023-02-01 18:28:25 +0530

    updated: footer copyright section

--- wp-content/themes/belmont_theme/footer.php
+++ wp-content/themes/belmont_theme/footer.php
@@ -1,16 +1,21 @@
     <footer class="global-footer">
         <div class="footer-logo">
             <?php $site_logo = get_field('site_logo', 'option'); ?>
-            <?php if(!empty($site_logo)):?>
-            <a href="<?php echo home_url( '/' ); ?>" rel="home" ><img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>"/></a>
-            <?php endif;?>
+            <?php if (!empty($site_logo)) : ?>
+                <a href="<?php echo home_url('/'); ?>" rel="home"><img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>" /></a>
+            <?php endif; ?>
         </div>
-        <nav aria-label="<?php _e( 'Footer Navigation', 'wpbp-theme' ); ?>">
-            <?php wp_nav_menu( array( 'container_class' => 'menu-footer', 'theme_location' => 'footer_menu' ) ); ?>
+        <nav aria-label="<?php _e('Footer Navigation', 'wpbp-theme'); ?>">
+            <?php wp_nav_menu(array('container_class' => 'menu-footer', 'theme_location' => 'footer_menu')); ?>
         </nav>
-        <?php $copyright_text = get_field('copyright_text', 'option');?>
-        <div class="footer-copyright"><?php echo $copyright_text;?></div>
+        <?php $copyright_text = get_field('copyright_text', 'option'); ?>
+        <div class="footer-copyright">
+            <?php if (!empty($copyright_text)) {
+                echo $copyright_text;
+            };?>
+        </div>
     </footer>
     <?php wp_footer(); ?>
     </body>
-</html>
+
+    </html>
