ca2b5b306e800297129b8a47eea06d5ae10fe897
emily.ritika <emily.ritika@rubicotech.in>
2023-02-02 17:13:02 +0530

    Updated simple content section class

--- wp-content/themes/belmont_theme/_layouts/simple_content_section.php
+++ wp-content/themes/belmont_theme/_layouts/simple_content_section.php
@@ -5,9 +5,9 @@ $section_content = get_sub_field('section_content');
 $select_section_width = get_sub_field('select_section_width');
 $upload_logo = get_sub_field('upload_logo');
 ?>
-<div class="simple-content-section <?php echo $select_section_width; ?>">
+<div class="simple-content-section <?php echo $select_section_width; ?> <?php echo $select_heading_type; ?>">
 <?php if($section_heading != '' || $upload_logo != '' ): ?><div class="heading-logo-wrap"><?php endif; ?>
-    <?php if($section_heading != ''): ?><h2 class="<?php echo $select_heading_type; ?>"><?php echo $section_heading; ?></h2><?php endif; ?>
+    <?php if($section_heading != ''): ?><h2><?php echo $section_heading; ?></h2><?php endif; ?>
     <?php if($upload_logo != ''): ?><img src="<?php echo $upload_logo['url']; ?>" alt="<?php echo $upload_logo['alt']; ?>" /><?php endif; ?>
 <?php if($section_heading != '' || $upload_logo != '' ): ?></div><?php endif; ?>
 <?php if($section_content):?><div class="content-wrapper"><?php echo $section_content; ?></div><?php endif; ?>
