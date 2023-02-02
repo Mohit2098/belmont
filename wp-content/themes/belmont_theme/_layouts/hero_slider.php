26ae16f79e0e5497a3f884b06dc6c2bde3e75bec
emily.ritika <emily.ritika@rubicotech.in>
2023-02-02 17:30:04 +0530

    Added gallery section and updated hero slider

--- wp-content/themes/belmont_theme/_layouts/hero_slider.php
+++ wp-content/themes/belmont_theme/_layouts/hero_slider.php
@@ -1,21 +1,16 @@
-   <section class="Hero-slider">
-      <div class="container">
-         <?php $content_alignment = get_sub_field('content_alignment'); ?>
+<section class="hero-slider">
          <?php if (have_rows('add_slides')) : ?>
             <div class="hero-slider-cl">
                <?php while (have_rows('add_slides')) : the_row();
                   $slide_heading = get_sub_field('heading');
                   $slide_description = get_sub_field('description');
                   $slide_cta = get_sub_field('add_cta');
-                  if ($slide_cta) {
-                     $button_url = $slide_cta['url'];
-                     $button_title = $slide_cta['title'];
-                     $button_target = $slide_cta['target'] ? $slide_cta['target'] : '_self';
-                  }
+                  $content_alignment = get_sub_field('content_alignment');
+                  $content_background_color = get_sub_field('content_background_color');
                   $column_image = get_sub_field('upload_column_image');
                   $slide_background_image = get_sub_field('slide_background_image');
                ?>
-                  <div class="wrapper">
+                  <div class="hero_wrapper">
                      <div>
                      <?php if (!empty($slide_background_image )) { ?>
                         <img src="<?php echo esc_url($slide_background_image ['url']); ?>" alt="<?php echo esc_attr($slide_background_image ['alt']); ?>" />
@@ -28,7 +23,11 @@
                      <?php if (!empty($slide_description)) {
                         echo "<div>" . $slide_description . "</div>";
                      } ?>
-                     <?php if (!empty($slide_cta)) { ?>
+                     <?php if (!empty($slide_cta)) {
+                        $button_url = $slide_cta['url'];
+                        $button_title = $slide_cta['title'];
+                        $button_target = $slide_cta['target'] ? $slide_cta['target'] : '_self';
+                     ?>
                         <a class="slide-button" href="<?php echo $button_url;?>" target="<?php echo $button_target; ?>">
                            <?php echo $button_title; ?>
                         </a>
@@ -42,5 +41,4 @@
                <?php endwhile; ?>
             </div>
          <?php endif; ?>
-      </div>
    </section>
