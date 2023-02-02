f94d3584b61ae1be3b63d6a83b2a253b2814757f
michael <michael.mustfa@ithands.net>
2023-02-02 18:42:35 +0530

    footer restructure and basic css for fonts and typography

--- wp-content/themes/belmont_theme/footer.php
+++ wp-content/themes/belmont_theme/footer.php
@@ -1,19 +1,33 @@
-    <footer class="global-footer">
-        <div class="footer-logo">
-            <?php $site_logo = get_field('site_logo', 'option'); ?>
-            <?php if (!empty($site_logo)) : ?>
-                <a href="<?php echo home_url('/'); ?>" rel="home"><img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>" /></a>
-            <?php endif; ?>
+    <footer class="global-footer bl-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="footer-logo">
+                        <?php $site_logo = get_field('site_logo', 'option'); ?>
+                        <?php if (!empty($site_logo)) : ?>
+                            <a href="<?php echo home_url('/'); ?>" rel="home"><img src="<?php echo esc_url($site_logo['url']); ?>" alt="<?php echo esc_attr($site_logo['alt']); ?>" /></a>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <!-- /col -->
+                <div class="col-lg-8">
+                    <nav aria-label="<?php _e('Footer Navigation', 'wpbp-theme'); ?>">
+                        <?php wp_nav_menu(array('container_class' => 'menu-footer', 'theme_location' => 'footer_menu')); ?>
+                    </nav>
+                </div>
+                <!-- /col -->
+                <div class="col-12">
+                    <?php $copyright_text = get_field('copyright_text', 'option'); ?>
+                    <div class="footer-copyright">
+                        <?php if (!empty($copyright_text)) {
+                            echo $copyright_text;
+                        };?>
+                    </div>
+                </div>
+                <!-- /col -->
+            </div>
         </div>
-        <nav aria-label="<?php _e('Footer Navigation', 'wpbp-theme'); ?>">
-            <?php wp_nav_menu(array('container_class' => 'menu-footer', 'theme_location' => 'footer_menu')); ?>
-        </nav>
-        <?php $copyright_text = get_field('copyright_text', 'option'); ?>
-        <div class="footer-copyright">
-            <?php if (!empty($copyright_text)) {
-                echo $copyright_text;
-            };?>
-        </div>
+       
     </footer>
     <?php wp_footer(); ?>
     </body>
