886637660745e98d685e117dfef1901f036b68cd
emily.ritika <emily.ritika@rubicotech.in>
2023-02-02 15:05:38 +0530

    Added Partners CPT and resp. fields

--- wp-content/themes/belmont_theme/_includes/functions-register.php
+++ wp-content/themes/belmont_theme/_includes/functions-register.php
@@ -15,11 +15,29 @@ function wpbp_register_menus() {
     'top_bar_menu' => 'Top Bar Menu',
 	) );
 }
-// 3. Register Custom Post Types
+// 2. Register Custom Post Types
+//Vendors CPT Starts
+add_action('init', 'belmont_cpt');
+function belmont_cpt() {
+  register_post_type('partners', array(
+      'labels' => array(
+        'name' => 'Partners',
+        'singular_name' => 'Partner'
+      ),
+      'public' => true,
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'show_in_nav_menus' => true,
+      "menu_icon" => "dashicons-businessman",
+      'rewrite' => array('slug' => 'partners'),
+      'supports' => array('title')
+    )
+  );
+}
 
-// 4. Sidebars
+// 3. Sidebars
 
-// 5. ACF Options
+// 4. ACF Options
 
 if( function_exists('acf_add_options_page') && current_user_can( 'manage_options' ) ) {
 	$parent = acf_add_options_page(array(
@@ -27,11 +45,5 @@ if( function_exists('acf_add_options_page') && current_user_can( 'manage_options
 		'menu_title' 	=> 'Options',
 		'redirect' 		=> false
 	));
-  /*
-  $child = acf_add_options_sub_page(array(
-      'page_title'  => __('Footer'),
-      'menu_title'  => __('Footer'),
-      'parent_slug' => $parent['menu_slug'],
-  ));
-  */
+
 }
