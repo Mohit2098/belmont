6161eb975c92e5acf901c6c4b8227ab242f655fa
melvin.mohit <melvin.mohit@rubicotech.in>
2023-02-02 17:43:10 +0530

    updated: function.php

--- wp-content/themes/belmont_theme/_includes/functions-enqueue.php
+++ wp-content/themes/belmont_theme/_includes/functions-enqueue.php
@@ -14,12 +14,18 @@
 
 // 1. Enqueue everything
 function wp_nq_scripts() {
+ 
+  //Styles
+
   wp_enqueue_style( 'style', get_stylesheet_uri(),null,1.0);
   wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri().'/_css/custom.css',null,1.0);
   wp_enqueue_style( 'layout-style', get_stylesheet_directory_uri().'/_css/layout.css',null,1.0);
-  wp_enqueue_style("slick-min-css","https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick.min.css",true);
-  wp_enqueue_style("slick-theme-min-css","https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick-theme.min.css",true);
-  wp_enqueue_style("adobe-fonts","https://use.typekit.net/nvw8hhc.css",true);
+  wp_enqueue_style("slick-min-css","https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick.min.css");
+  wp_enqueue_style("slick-theme-min-css","https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.5.9/slick-theme.min.css");
+  wp_enqueue_style("adobe-fonts","https://use.typekit.net/nvw8hhc.css");
+
+  //Scripts
+
   wp_enqueue_script("slick-min-js","https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js",["jquery"],"",true);
   wp_enqueue_script ('wp-script', get_stylesheet_directory_uri() . '/_js/js.js', array('jquery'), 1.0, true);
 }
