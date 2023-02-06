cddbcf1f657703bb37f4e81bc5fd52f44dda92f3
melvin.mohit <melvin.mohit@rubicotech.in>
2023-02-06 13:04:15 +0530

    updated: hero slider and two_column_grid_section with dynamic structure

--- wp-content/themes/belmont_theme/_layouts/hero_slider.php
+++ wp-content/themes/belmont_theme/_layouts/hero_slider.php
@@ -1,129 +1,51 @@
+<?php if (have_rows('add_slides')) : ?>
 <section class="hero-slider">
-         <?php if (have_rows('add_slides')) : ?>
-            <div class="hero-slider-cl">
-               <?php while (have_rows('add_slides')) : the_row();
-                  $slide_heading = get_sub_field('heading');
-                  $slide_description = get_sub_field('description');
-                  $slide_cta = get_sub_field('add_cta');
-                  $content_alignment = get_sub_field('content_alignment');
-                  $content_background_color = get_sub_field('content_background_color');
-                  $column_image = get_sub_field('upload_column_image');
-                  $slide_background_image = get_sub_field('slide_background_image');
-               ?>
-                  <div class="hero_wrapper">
-                     <div>
-                     <?php if (!empty($slide_background_image )) { ?>
-                        <img src="<?php echo esc_url($slide_background_image ['url']); ?>" alt="<?php echo esc_attr($slide_background_image ['alt']); ?>" />
-                     <?php } ?>
-                     </div>
-                  <div class="select-drop-down <?php $content_alignment == 'left' ? 'align-left' : 'align-right'; ?>">
-                     <?php if (!empty($slide_heading)) {
-                        echo "<h1>" . $slide_heading . "</h1>";
-                     } ?>
-                     <?php if (!empty($slide_description)) {
-                        echo "<div>" . $slide_description . "</div>";
-                     } ?>
-                     <?php if (!empty($slide_cta)) {
-                        $button_url = $slide_cta['url'];
-                        $button_title = $slide_cta['title'];
-                        $button_target = $slide_cta['target'] ? $slide_cta['target'] : '_self';
-                     ?>
-                        <a class="slide-button" href="<?php echo $button_url;?>" target="<?php echo $button_target; ?>">
-                           <?php echo $button_title; ?>
-                        </a>
-                     <?php } ?>
-                     <?php if (!empty($column_image)) { ?>
-                        <img src="<?php echo esc_url($column_image['url']); ?>" alt="<?php echo esc_attr($column_image['alt']); ?>" />
-                     <?php } ?>
-                  </div>
-                  </div>
-                  
-               <?php endwhile; ?>
-            </div>
-         <?php endif; ?>
-
-
    <div class="hero-slider-bl">
-      <div class="items-hero">
-          <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" class="banner-back" alt="">    
-          <div class="container">
-            <div class="row">
-               <div class="col-lg-6">
-                  <div class="caption-wrap">
-                        <h1>proven performance</h1>
-                        <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form</p>
-                        <a href="#" class="btn-custom outline-yellow">All trailer models</a>
+         <?php while (have_rows('add_slides')) : the_row();
+            $slide_heading = get_sub_field('heading');
+            $slide_description = get_sub_field('description');
+            $slide_cta = get_sub_field('add_cta');
+            $content_alignment = get_sub_field('content_alignment');
+            $content_background_color = get_sub_field('content_background_color');
+            $column_image = get_sub_field('upload_column_image');
+            $slide_background_image = get_sub_field('slide_background_image');
+         ?>
+         <div class="items-hero <?php echo esc_attr($content_background_color); ?> <?php echo $content_alignment == 'left' ? 'align-left' : 'align-right'; ?>">
+               <?php if (!empty($slide_background_image)) { ?>
+                  <img class="banner-back" src="<?php echo esc_url($slide_background_image['url']); ?>" alt="<?php echo esc_attr($slide_background_image['alt']); ?>" />
+               <?php } ?>
+               <div class="container">
+                  <div class="row">
+                     <div class="col-lg-6">
+                        <div class="caption-wrap">
+                        <?php if (!empty($slide_heading)):?><h1><?php echo $slide_heading; ?><?php endif; ?></h1>
+                          <?php if (!empty($slide_description)):?><div><?php echo $slide_description;?></div><?php endif;?>
+                           <?php if (!empty($slide_cta)):?>
+                              <?php 
+                              $button_url = $slide_cta['url'];
+                              $button_title = $slide_cta['title'];
+                              $button_target = $slide_cta['target'] ? $slide_cta['target'] : '_self';
+                           ?>
+                              <a class="btn-custom outline-yellow" href="<?php echo $button_url; ?>" target="<?php echo $button_target; ?>">
+                                 <?php echo $button_title; ?>
+                              </a>
+                           <?php endif;?>
+                        </div>
+                     </div>
+                     <!-- /col -->
+                     <div class="col-lg-6">
+                        <?php if (!empty($column_image)):?>
+                           <img src="<?php echo esc_url($column_image['url']); ?>" alt="<?php echo esc_attr($column_image['alt']); ?>" />
+                        <?php endif; ?>
+                     </div>
+                     <!-- /col -->
                   </div>
+                  <!-- /row -->
                </div>
-               <!-- /col -->
-               <div class="col-lg-6">
-                  <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt="">   
-               </div>
-               <!-- /col -->
+                <!-- /container -->
             </div>
-          </div>        
-      </div>
-      <!-- /items -->
-      <div class="items-hero">
-          <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" class="banner-back" alt="">    
-          <div class="container">
-            <div class="row">
-               <div class="col-lg-6">
-                  <div class="caption-wrap">
-                        <h1>proven performance</h1>
-                        <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form</p>
-                        <a href="#" class="btn-custom outline-yellow">All trailer models</a>
-                  </div>
-               </div>
-               <!-- /col -->
-               <div class="col-lg-6">
-                  <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt="">   
-               </div>
-               <!-- /col -->
-            </div>
-          </div>        
-      </div>
-      <!-- /items -->
-      <div class="items-hero">
-          <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" class="banner-back" alt="">    
-          <div class="container">
-            <div class="row">
-               <div class="col-lg-6">
-                  <div class="caption-wrap">
-                        <h1>proven performance</h1>
-                        <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form</p>
-                        <a href="#" class="btn-custom outline-yellow">All trailer models</a>
-                  </div>
-               </div>
-               <!-- /col -->
-               <div class="col-lg-6">
-                  <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt="">   
-               </div>
-               <!-- /col -->
-            </div>
-          </div>        
-      </div>
-      <!-- /items -->
-      <div class="items-hero">
-          <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" class="banner-back" alt="">    
-          <div class="container">
-            <div class="row">
-               <div class="col-lg-6">
-                  <div class="caption-wrap">
-                        <h1>proven performance</h1>
-                        <p>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form</p>
-                        <a href="#" class="btn-custom outline-yellow">All trailer models</a>
-                  </div>
-               </div>
-               <!-- /col -->
-               <div class="col-lg-6">
-                  <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt="">   
-               </div>
-               <!-- /col -->
-            </div>
-          </div>        
-      </div>
-      <!-- /items -->
+             <!-- /items-hero -->
+         <?php endwhile; ?>
    </div>
-
-   </section>
+</section>
+<?php endif; ?>
--- wp-content/themes/belmont_theme/_layouts/two_column_grid_section.php
+++ wp-content/themes/belmont_theme/_layouts/two_column_grid_section.php
@@ -1,112 +1,55 @@
 <section class="two-column-grid-section">
-    <div class="container-fluid">
-        <div class="text-center">
+    <!-- new structure -->
+    <div class="container">
+    <div class="text-center">
             <?php $section_heading = get_sub_field('section_heading'); ?>
-            <?php if (!empty($section_heading)) {
-                echo "<h1>" . $section_heading . "</h1>";
-            } ?>
+            <?php if (!empty($section_heading)):?>
+                <h1> <?php echo $section_heading;?></h1>";
+            <?php endif;?>
         </div>
+        <div class="row">
         <?php if (have_rows('grid_content')) : ?>
-            <div class="row">
                 <?php while (have_rows('grid_content')) : the_row();
                     $image = get_sub_field('image');
                     $heading = get_sub_field('heading');
                     $description = get_sub_field('description');
                     $add_cta = get_sub_field('add_cta');
                 ?>
-                    <div class="col-lg-6 justify-content-center d-grid">
-                        <a href="<?php echo $button_url; ?>">
-                            <?php if (!empty($image)) { ?>
-                                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" style="width:100px" />
-                            <?php } ?>
-
-                            <?php if (!empty($heading)) {
-                                echo "<h1 style='font-size:14px'>" . $heading . "</h1>";
-                            } ?>
-
-                            <?php if (!empty($description)) {
-                                echo "<h2 style='font-size:14px'>" . $description . "</h2>";
-                            } ?>
-                            <?php if (!empty($add_cta)) {
+            <div class="col-lg-6">
+                <div class="d-flex flex-wrap">
+                <?php if (!empty($image)): ?>
+                                <img class="banner-back" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"/>
+                            <?php endif;?>
+                    <div class="content-wrapper">
+                       <?php if (!empty($heading)) :?><h3><?php echo $heading;?><?php endif;?></h3>
+                       <?php if (!empty($description)):?><div> <?php echo $description;?></div><?php endif?>
+                               <?php if (!empty($add_cta)):?>
+                                <?php 
                                 $button_url = $add_cta['url'];
                                 $button_title = $add_cta['title'];
                                 $button_target = $add_cta['target'] ? $add_cta['target'] : '_self';
-                            ?>
-                                <a class="button" href="<?php echo $button_url; ?>" target="<?php echo $button_target; ?>">
+                                ?>
+                                <a class="learn-more" href="<?php echo $button_url; ?>" target="<?php echo $button_target; ?>">
                                     <?php echo $button_title; ?>
                                 </a>
-                        </a>
-                    <?php } ?>
+                    <?php endif; ?>
                     </div>
-                <?php endwhile; ?>
+                </div>
             </div>
-        <?php endif; ?>
-</section>
-<section class="content-section">
-    <div class="text-center mt-4 mb-4">
-        <?php $section_cta = get_sub_field('section_cta'); ?>
-        <?php if (!empty($section_cta)) { ?>
+            <?php endwhile; ?>
+                <?php endif; ?>
+            <!-- /col -->
+            <div class="col-12 text-center">
+            <?php $section_cta = get_sub_field('section_cta'); ?>
+        <?php if (!empty($section_cta)) : ?>
             <?php
             $button_url = $section_cta['url'];
             $button_title = $section_cta['title'];
             $button_target = $section_cta['target'] ? $section_cta['target'] : '_self'; ?>
-            <a class="button" href="<?php echo $button_url; ?>" target="<?php echo $button_target; ?>">
+            <a class="btn-custom" href="<?php echo $button_url; ?>" target="<?php echo $button_target; ?>">
                 <?php echo $button_title; ?>
             </a>
-        <?php } ?>
-    </div>
-    </div>
-
-
-    <!-- new structure -->
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="d-flex flex-wrap">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/Warranty.svg" class="banner-back" alt="">
-                    <div class="content-wrapper">
-                        <h3>Belmont Warranty</h3>
-                        <p>One of the best in the industry.</p>
-                        <a href="#" class="learn-more">READ MORE</a>
-                    </div>
-                </div>
-            </div>
-            <!-- /col -->
-            <div class="col-lg-6">
-                <div class="d-flex flex-wrap">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/Warranty.svg" class="banner-back" alt="">
-                    <div class="content-wrapper">
-                        <h3>Belmont Warranty</h3>
-                        <p>One of the best in the industry.</p>
-                        <a href="#" class="learn-more">READ MORE</a>
-                    </div>
-                </div>
-            </div>
-            <!-- /col -->
-            <div class="col-lg-6">
-                <div class="d-flex flex-wrap">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/Warranty.svg" class="banner-back" alt="">
-                    <div class="content-wrapper">
-                        <h3>Belmont Warranty</h3>
-                        <p>One of the best in the industry.</p>
-                        <a href="#" class="learn-more">READ MORE</a>
-                    </div>
-                </div>
-            </div>
-            <!-- /col -->
-            <div class="col-lg-6">
-                <div class="d-flex flex-wrap">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/Warranty.svg" class="banner-back" alt="">
-                    <div class="content-wrapper">
-                        <h3>Belmont Warranty</h3>
-                        <p>One of the best in the industry.</p>
-                        <a href="#" class="learn-more">READ MORE</a>
-                    </div>
-                </div>
-            </div>
-            <!-- /col -->
-            <div class="col-12 text-center">
-                <a href="#" class="btn-custom">See Trailer Models</a>
+        <?php endif; ?>
             </div>
         </div>
     </div>
