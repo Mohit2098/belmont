95e802e67df468ecde4cb1f36ca649c354ce69d1
michael <michael.mustfa@ithands.net>
2023-02-06 13:11:09 +0530

    structure for forms and trailers

--- wp-content/themes/belmont_theme/_layouts/all_trailers_section.php
+++ wp-content/themes/belmont_theme/_layouts/all_trailers_section.php
@@ -1,1 +1,30 @@
-All Trailers
+<section class="all-trailers-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="inner-white-box d-flex flex-wrap">
+                    <div class="caption">
+                        <h3>Tube Top Utility Trailers</h3>
+                        <a href="#" class="btn-custom-small">SEE TRAILER SPECS</a>
+                    </div>
+                    <div class="img-box">
+                        <img class="trailer-img" src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- /col -->
+            <div class="col-lg-6">
+                <div class="inner-white-box d-flex flex-wrap">
+                    <div class="caption">
+                        <h3>Tube Top Utility Trailers</h3>
+                        <a href="#" class="btn-custom-small">SEE TRAILER SPECS</a>
+                    </div>
+                    <div class="img-box">
+                        <img class="trailer-img" src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <!-- /col -->
+        </div>
+    </div>
+</section>
