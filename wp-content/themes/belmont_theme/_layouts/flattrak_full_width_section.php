b8263cba80b33c5e1208bd33549355e05af33416
michael <michael.mustfa@ithands.net>
2023-02-09 13:00:41 +0530

    update: restructure for the flattrack three up and full width section

--- wp-content/themes/belmont_theme/_layouts/flattrak_full_width_section.php
+++ wp-content/themes/belmont_theme/_layouts/flattrak_full_width_section.php
@@ -4,14 +4,18 @@ $add_cta = get_sub_field('add_cta');
 ?>
 <section class="flattrak-full-width-section">
     <div class="container">
-        <?php if (!empty($logo)) : ?>
-            <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>" />
-        <?php endif; ?>
-        <?php if (!empty($add_cta)) :
-            $button_target = $add_cta['target'] ? $add_cta['target'] : '_self'; ?>
-            <a href="<?php echo $add_cta['url']; ?>" target="<?php echo $button_target; ?>">
-                <?php echo $add_cta['title']; ?>
-            </a>
-        <?php endif; ?>
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                <?php if (!empty($logo)) : ?>
+                <img src="<?php echo esc_url($logo['url']); ?>" alt="<?php echo esc_attr($logo['alt']); ?>" />
+                <?php endif; ?>
+                <?php if (!empty($add_cta)) :
+                    $button_target = $add_cta['target'] ? $add_cta['target'] : '_self'; ?>
+                    <a href="<?php echo $add_cta['url']; ?>" target="<?php echo $button_target; ?>">
+                        <?php echo $add_cta['title']; ?>
+                    </a>
+                <?php endif; ?>
+            </div>
+        </div>
     </div>
 </section>
--- wp-content/themes/belmont_theme/_layouts/flattrak_three_up_section.php
+++ wp-content/themes/belmont_theme/_layouts/flattrak_three_up_section.php
@@ -1,14 +1,25 @@
 <section class="flattrak-three-up-section">
     <div class="container">
-        <?php if (have_rows('flattrak_three_up_content')) : ?>
-            <?php
-            while (have_rows('flattrak_three_up_content')) : the_row();
-                $heading_bold = get_sub_field('heading_bold');
-                $description = get_sub_field('description');
-            ?>
-                <?php if (!empty($heading_bold)) : ?><h1><?php echo $heading_bold; ?></h1><?php endif; ?>
-                <?php if (!empty($description)) : ?><h2><?php echo $description; ?></h2><?php endif; ?>
-            <?php endwhile; ?>
-        <?php endif; ?>
+        
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="inner-box">
+                    <h3><strong>Strong </strong>as steel</h3>
+                </div>
+            </div>
+            <!-- /col -->
+            <div class="col-lg-4">
+                <div class="inner-box">
+                    <h3><strong>Safety  </strong>first</h3>
+                </div>
+            </div>
+            <!-- /col -->
+            <div class="col-lg-4">
+                <div class="inner-box">
+                    <h3><strong>Strong </strong>as steel</h3>
+                </div>
+            </div>
+            <!-- /col -->
+        </div>
     </div>
 </section>
