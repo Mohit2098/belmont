e1d58007c801689f682ccedc7a1b36f8824b1a37
michael <michael.mustfa@ithands.net>
2023-02-09 11:55:23 +0530

    add: single trailers structure

--- wp-content/themes/belmont_theme/archive.php
+++ wp-content/themes/belmont_theme/archive.php
@@ -1,21 +1,187 @@
 <?php get_header(); ?>
 <main id="content" class="category-page trailer-archive">
-		<header>
-			<h1 class="entry-title"><?php single_post_title();; ?></h1>
-		</header>
-		<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		<article id="post-<?php the_ID(); ?>" class="wp-a11y-card">
-			<header>
-				<h2>
-					<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-				</h2>
-			</header>
-			<?php the_excerpt(); ?>
-    </article>
-		<?php endwhile; ?>
-		<footer>
-			<?php wpbp_pagination($pages = '', $range = 2,"Posts"); ?>
-		</footer>
+	<section class="bl-section single-trailers-section">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-6">
+					<h1 class="text-medium">Single Axle Utility Trailers</h1>
+					<strong class="h3">5’ x 8’ | 5’ x 10’ | 6’ x 10’ | 6’ x 12’ | 6’ x 14’</strong>
+				</div>
+				<!-- /col -->
+				<div class="col-lg-6">
+					<div class="tab-refresh">
+						<a href="#" class="tabs-link active">Tube Top Trailers</a>
+						<a href="#" class="tabs-link">Solid Side Trailers</a>
+					</div>
+				</div>
+				<!-- /col -->
+			</div>
+		</div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="trailer-slide-wrap">
+						<div class="slider-product">
+							<div class="items-product">
+							   <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="items-product">
+							   <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="items-product">
+							   <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="items-product">
+							   <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="items-product">
+							   <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="items-product">
+							   <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt=""> 
+							</div>
+							<!-- /item -->
+						</div>
+						<!-- /upper slider -->
+						<div class="products-nav">
+							<div class="item-nav">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="item-nav">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="item-nav">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/_images/dummy.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="item-nav">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="item-nav">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 
+							</div>
+							<!-- /item -->
+							<div class="item-nav">
+								<img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 
+							</div>
+							<!-- /item -->
+						</div>
+						<!-- slider lower -->
+					</div>
+				</div>
+				<!-- /col -->
+				<div class="col-lg-6">
+					<div class="description">
+						<h2 class="text-medium">Tube Top Utility Trailers</h2>
+						<p>If you’re seeking a rugged, high-quality utility trailer, look no further. Built for daily use, this trailer is great for hauling around your ATV, commercial mower, or the host of other things that come up. This model series has been a bestseller for years and continues to be admired for a host of features and benefits not easily found elsewhere.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="details-section-trailer">
+		<div class="container">
+			<div class="detail-box">
+				<h2 class="text-medium">UT610TT – 6’ x 10’ Utility Trailer</h2>
+				<div class="row">
+					<div class="col-lg-6">
+					  <img src="<?php echo get_stylesheet_directory_uri() ?>/_images/trailer.png" alt=""> 	
+					  <div class="table-wrap">
+						<table cellspacing="0">
+							<thead>
+								<tr>
+									<th>GVWR</th>
+									<th>EMPTY WEIGHT</th>
+									<th>PAYLOAD	</th>
+									<th>BED SIZE</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>2999</td>
+									<td>2999</td>
+									<td>2999</td>
+									<td>76” X 12’</td>
+								</tr>
+							</tbody>
+						</table>
+						<!-- /table -->
+						<table cellspacing="0">
+							<thead>
+								<tr>
+									<th>GVWR</th>
+									<th>EMPTY WEIGHT</th>
+									<th>PAYLOAD	</th>
+									<th>BED SIZE</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>2999</td>
+									<td>2999</td>
+									<td>2999</td>
+									<td>76” X 12’</td>
+								</tr>
+							</tbody>
+						</table>
+						<!-- /table -->
+					  </div>
+					</div>
+					<!-- /col -->
+					<div class="col-lg-6">
+						<div class="feature-list">
+							<h3>Standard Features</h3>
+							<a href="#" class="btn-custom-small solid-yellow">IN COMPARE</a>
+							<a href="#" class="btn-custom-small solid-yellow">IN COMPARE</a>
+							<ul>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+							</ul>
+							<ul class="toggle-content">
+							    <li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+								<li>Heavy-Duty Tube Top Rail and Uprights • 12</li>
+							</ul>
+							<a href="javascript:void(0)" class="trigger-toggle"><span class="more">Show more</span><span class="less">Show less</span>  </a>
+						</div>
+					</div>
+					<!-- /col -->
+					<div class="col-12">
+						<h3><a href="javascript:void(0)" class="option-toggle">Available Options</a></h3>
+						<div class="options-content">
+							<ul>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+								<li>Hot Dipped Galvanized Finish • A-Frame Toolbox</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+		
 </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
