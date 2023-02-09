317d4a634afe6a29ce7d3029812b388fc6387550
michael <michael.mustfa@ithands.net>
2023-02-09 17:09:34 +0530

    add: flat track hero style

--- wp-content/themes/belmont_theme/_layouts/flattrak_hero_section.php
+++ wp-content/themes/belmont_theme/_layouts/flattrak_hero_section.php
@@ -3,15 +3,15 @@ $hero_background_image = get_sub_field('hero_background_image');
 $hero_cta = get_sub_field('hero_cta');
 ?>
 <section class="flattrak-hero-section">
-    <div class="container">
+    <div class="hero-img">
         <?php if (!empty($hero_background_image)) : ?>
             <img src="<?php echo esc_url($hero_background_image['url']); ?>" alt="<?php echo esc_attr($hero_background_image['alt']); ?>" />
         <?php endif; ?>
-        <?php if (!empty($hero_cta)) :
+    </div>
+    <?php if (!empty($hero_cta)) :
             $button_target = $hero_cta['target'] ? $hero_cta['target'] : '_self'; ?>
-            <a href="<?php echo $hero_cta['url']; ?>" target="<?php echo $button_target; ?>">
+            <a href="<?php echo $hero_cta['url']; ?>" class="btn-custom" target="<?php echo $button_target; ?>">
                 <?php echo $hero_cta['title']; ?>
             </a>
         <?php endif; ?>
-    </div>
 </section>
