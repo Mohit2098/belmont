1924e8c5b4a8cd2a681c8f52db89f7f27e8faacd
melvin.mohit <melvin.mohit@rubicotech.in>
2023-02-14 11:26:14 +0530

    restructured testimonial section

--- wp-content/themes/belmont_theme/_layouts/flattrak_testimonial_section.php
+++ wp-content/themes/belmont_theme/_layouts/flattrak_testimonial_section.php
@@ -1,11 +1,18 @@
 <section class="bl-section flattrak-testimonial">
     <div class="container">
         <div class="row justify-content-center">
+            <div class="col-12">
+                <?php $flattrak_testimonial_logo = get_sub_field('flattrak_testimonial_logo'); ?>
+                <?php if (!empty($flattrak_testimonial_logo)) : ?>
+                    <img class="quote-img" src="<?php echo esc_url($flattrak_testimonial_logo['url']); ?>" alt="<?php echo esc_attr($flattrak_testimonial_logo['alt']); ?>" />
+                <?php endif; ?>
+            </div>
             <div class="col-12 testimonial-slider">
+
                 <?php if (have_rows('flattrak_testimonial_content')) : ?>
                     <?php
                     while (have_rows('flattrak_testimonial_content')) : the_row();
-                        $flattrak_testimonial_logo = get_sub_field('flattrak_testimonial_logo');
+
                         $add_testimonial_content = get_sub_field('add_testimonial_content');
                         $author_name = get_sub_field('name');
                         $company_name = get_sub_field('company');
@@ -13,9 +20,7 @@
                     ?>
                         <div class="items">
                             <div class="testi-content">
-                                <?php if (!empty($flattrak_testimonial_logo)) : ?>
-                                    <img class="quote-img" src="<?php echo esc_url($flattrak_testimonial_logo['url']); ?>" alt="<?php echo esc_attr($flattrak_testimonial_logo['alt']); ?>" />
-                                <?php endif; ?>
+
                                 <?php if (!empty($add_testimonial_content)) : ?><div><?php echo $add_testimonial_content; ?></div><?php endif; ?>
                                 <?php if (!empty($author_name)) : ?><span class="author-name"><?php echo $author_name; ?></span><?php endif; ?>
                                 <?php if (!empty($company_name)) : ?><span class="company-name"><?php echo $company_name; ?></span><?php endif; ?>
