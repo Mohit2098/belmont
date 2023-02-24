8aba349a605d7c327d67acfe8f3ece9b87efbcd7
michael <michael.mustfa@ithands.net>
2023-02-24 15:58:55 +0530

    privacy policy page style and dealer locator ipad

--- wp-content/themes/belmont_theme/page.php
+++ wp-content/themes/belmont_theme/page.php
@@ -1,14 +1,22 @@
 <?php get_header(); ?>
 <main id="content">
 	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
-		</header>
-		<div class="entry-content">
-			<?php the_content(); ?>
+	<section class="bl-section bl-default-template">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<header>
+							<h1 class="entry-title"><?php the_title(); ?></h1>
+						</header>
+						<div class="entry-content">
+							<?php the_content(); ?>
+						</div>
+					</article>
+				</div>
+			</div>
 		</div>
-	</article>
+	</section>
 	<?php endwhile; ?>
 </main>
 <?php get_sidebar(); ?>
