7c4d0268516ba40fecc68ac50f79615e4865d9a3
michael <michael.mustfa@ithands.net>
2023-05-10 13:14:51 +0530

    add: structure for the coaparison table

--- wp-content/themes/belmont_theme/incompare.php
+++ wp-content/themes/belmont_theme/incompare.php
@@ -5,23 +5,101 @@
 
 get_header();
 ?>
-<?php
-if (isset($_COOKIE['trailerCookies'])) {
-    $trailerCookie = $_COOKIE['trailerCookies'];
-    $cookieArr = explode(",", $trailerCookie);
-    array_pop($cookieArr);
-
-    // Add counter variable
-    $count = 0;
-    echo ' <input type="checkbox" id="">';
-    echo '<div class="row">';
-    for ($count = 0; $count < count($cookieArr); $count++) : ?>
-
-        <div class="container">
-            <?php $featured = get_field('trailer_featured_image', $cookieArr[$count]); ?>
-            <img src="<?php echo $featured['url'] ?>" style="height: 100px; width: 100px;">
+<section class="in-compare-section bl-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-medium">Comparison results</h2>
+                <table class="comparison-table" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th> <input type="checkbox" name="high" > Highlight Differences</th>
+                            <th class="img-head"><a href="#"> <img class="comp-trailer" width="150" hight="100" src="<?php echo get_template_directory_uri(); ?>/_images/trailer.png" /> <span>UT612TT</span> </a> </th>
+                            <th class="img-head"><a href="#"> <img class="comp-trailer" width="150" hight="100" src="<?php echo get_template_directory_uri(); ?>/_images/trailer.png" /> <span>UT445TT</span> </a> </th>
+                            <th class="img-head"><a href="#"> <img class="comp-trailer" width="150" hight="100" src="<?php echo get_template_directory_uri(); ?>/_images/trailer.png" /> <span>UT425FT</span> </a> </th>
+                            <th class="img-head"><a href="#"> <img class="comp-trailer" width="150" hight="100" src="<?php echo get_template_directory_uri(); ?>/_images/trailer.png" /> <span>UT745TT</span> </a> </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="gray-bg-td" colspan="5">Hardware Specifications</td>
+                        </tr>
+                        <tr>
+                            <td class="gray-lt-bg-td" colspan="5">CPU</td>
+                        </tr> 
+                        <tr>
+                            <td>CPU Model</td>
+                            <td>AMD Ryzen V1780B</td>
+                            <td>Intel Xeon D-1541</td>
+                            <td>Intel Xeon D-1567</td>
+                            <td>Intel Xeon D-1567</td>
+                        </tr>  
+                        <tr>
+                            <td>CPU Quantity</td>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>1</td>
+                            <td>4</td>
+                        </tr> 
+                        <tr>
+                            <td>CPU Model</td>
+                            <td>AMD Ryzen V1780B</td>
+                            <td>Intel Xeon D-1541</td>
+                            <td>Intel Xeon D-1567</td>
+                            <td>Intel Xeon D-1567</td>
+                        </tr>  
+                        <tr>
+                            <td class="gray-lt-bg-td" colspan="5">Memory</td>
+                        </tr> 
+                        <tr>
+                            <td>CPU Model</td>
+                            <td>AMD Ryzen V1780B</td>
+                            <td>Intel Xeon D-1541</td>
+                            <td>Intel Xeon D-1567</td>
+                            <td>Intel Xeon D-1567</td>
+                        </tr> 
+                        <tr>
+                            <td>CPU Model</td>
+                            <td>AMD Ryzen V1780B</td>
+                            <td>Intel Xeon D-1541</td>
+                            <td>Intel Xeon D-1567</td>
+                            <td>Intel Xeon D-1567</td>
+                        </tr> 
+                        <tr>
+                            <td>Note</td>
+                            <td>
+                                <ul>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                </ul>
+                            </td>
+                            <td>
+                            <ul>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                </ul>
+                            </td>
+                            <td>
+                            <ul>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                </ul>
+                            </td>
+                            <td>
+                            <ul>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                    <li>Synology reserves the right to replace memory modules with the same or higher frequency based on supplier's product life cycle status. Rest assured that the compatibility and stability have been</li>
+                                </ul>
+                            </td>
+                        </tr> 
+                    </tbody>
+                </table>
+            </div>
         </div>
-<?php
-    endfor;
-} ?>
+    </div>
+</section>
 <?php get_footer(); ?>
