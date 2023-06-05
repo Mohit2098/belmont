63aba321cede9798604f2cc7eab63ee4309cd68a
michael <michael.mustfa@ithands.net>
2023-06-05 12:25:42 +0530

    update: comaprison table style and responisivness

--- wp-content/themes/belmont_theme/comparison-results.php
+++ wp-content/themes/belmont_theme/comparison-results.php
@@ -14,130 +14,132 @@ get_header();
             <div class="row">
                 <div class="col-12">
                     <h2 class="text-medium"><?php _e('Comparison results', 'btrailers-theme'); ?></h2>
-                    <table class="comparison-table" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th> <input type="checkbox" name="high" id="highlight_diff"><?php _e('Highlight Differences', 'btrailers-theme'); ?></th>
-                                <?php if (!empty($cookieArr)) {
-                                    for ($i = 0; $i < count($cookieArr); $i++) :
-                                        $featured = get_field('trailer_featured_image', $cookieArr[$i]);
-                                        $terms = get_the_terms($cookieArr[$i], 'trailers');
-                                        $termLink = '';
-                                        $tabID = '';
-
-                                        if ($terms[0]->parent != 0) {
-                                            $termLink = get_term_link($terms[0]->parent);
-                                            $tabID = $terms[0]->term_id;
-                                        } else {
-                                            $termLink = get_term_link($terms[1]->parent);
-                                            $tabID = $terms[1]->term_id;
-                                        }
-                                        $postLink = $termLink;
-                                        $activeClass = (isset($_COOKIE['tabCookie']) && $_COOKIE['tabCookie'] == $tabID) ? 'active' : '';
-                                        echo '<th class="img-head"><a href="' . $postLink . '" data-tabid="' . $tabID . '" class="tabContentCl' . $activeClass . '"> <img class="comp-trailer" width="150" height="100" src="' . $featured['url'] . '" /> <span>' . get_the_title($cookieArr[$i]) . '</span> </a> </th>';
-                                ?>
-                                <?php endfor;
-                                } ?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (!empty($rowcount)) : ?>
+                    <div class="table-wrap-comparison">
+                        <table class="comparison-table" cellspacing="0">
+                            <thead>
                                 <tr>
-                                    <td class="gray-bg-td" colspan="<?php echo $rowcount + 1; ?>"><?php _e('Trailers Specifications', 'btrailers-theme'); ?></td>
+                                    <th> </th>
+                                    <?php if (!empty($cookieArr)) {
+                                        for ($i = 0; $i < count($cookieArr); $i++) :
+                                            $featured = get_field('trailer_featured_image', $cookieArr[$i]);
+                                            $terms = get_the_terms($cookieArr[$i], 'trailers');
+                                            $termLink = '';
+                                            $tabID = '';
+
+                                            if ($terms[0]->parent != 0) {
+                                                $termLink = get_term_link($terms[0]->parent);
+                                                $tabID = $terms[0]->term_id;
+                                            } else {
+                                                $termLink = get_term_link($terms[1]->parent);
+                                                $tabID = $terms[1]->term_id;
+                                            }
+                                            $postLink = $termLink;
+                                            $activeClass = (isset($_COOKIE['tabCookie']) && $_COOKIE['tabCookie'] == $tabID) ? 'active' : '';
+                                            echo '<th class="img-head"><a href="' . $postLink . '" data-tabid="' . $tabID . '" class="tabContentCl' . $activeClass . '"> <img class="comp-trailer" width="150" height="100" src="' . $featured['url'] . '" /> <span>' . get_the_title($cookieArr[$i]) . '</span> </a> </th>';
+                                    ?>
+                                    <?php endfor;
+                                    } ?>
                                 </tr>
-                            <?php endif; ?>
+                            </thead>
+                            <tbody>
+                                <?php if (!empty($rowcount)) : ?>
+                                    <tr>
+                                        <td class="yellow-bg-td" colspan="<?php echo $rowcount + 1; ?>"><div class="inner-td-wrap"> <?php _e('Trailers Specifications', 'btrailers-theme'); ?></div></td>
+                                    </tr>
+                                <?php endif; ?>
 
-                            <!-- Trailer Attribute  -->
-                            <?php if (!empty($rowcount)) : ?>
-                                <tr>
-                                    <td class="gray-lt-bg-td" colspan="<?php echo $rowcount + 1; ?>"><?php _e('Trailer Attribute', 'btrailers-theme'); ?></td>
-                                </tr>
-                            <?php endif;
+                                <!-- Trailer Attribute  -->
+                                <?php if (!empty($rowcount)) : ?>
+                                    <tr>
+                                        <td class="gray-lt-bg-td" colspan="<?php echo $rowcount + 1; ?>"><div class="inner-td-wrap"> <?php _e('Trailer Attribute', 'btrailers-theme'); ?> </div></td>
+                                    </tr>
+                                <?php endif;
 
-                            if (!empty($cookieArr)) {
-                                $titles = array();
-                                $details = array();
-
-                                for ($i = 0; $i < count($cookieArr); $i++) {
-                                    $add_attribute_rows = get_field('add_trailer_attribute', $cookieArr[$i]);
-
-                                    foreach ($add_attribute_rows as $add_attribute_row) {
-                                        $add_attribute_title = $add_attribute_row['add_attribute_title'];
-                                        $add_attribute_detail = $add_attribute_row['add_attribute_detail'];
-
-                                        if (!in_array($add_attribute_title, $titles)) {
-                                            array_push($titles, $add_attribute_title);
-                                        }
-
-                                        $details[$i][$add_attribute_title] = $add_attribute_detail;
-                                    }
-                                }
-                                foreach ($titles as $title) {
-                                    echo '<tr>';
-                                    echo '<td>' . __($title) . '</td>';
+                                if (!empty($cookieArr)) {
+                                    $titles = array();
+                                    $details = array();
 
                                     for ($i = 0; $i < count($cookieArr); $i++) {
-                                        $detail = isset($details[$i][$title]) ? $details[$i][$title] : '-';
-                                        echo '<td>' . __($detail) . '</td>';
+                                        $add_attribute_rows = get_field('add_trailer_attribute', $cookieArr[$i]);
+
+                                        foreach ($add_attribute_rows as $add_attribute_row) {
+                                            $add_attribute_title = $add_attribute_row['add_attribute_title'];
+                                            $add_attribute_detail = $add_attribute_row['add_attribute_detail'];
+
+                                            if (!in_array($add_attribute_title, $titles)) {
+                                                array_push($titles, $add_attribute_title);
+                                            }
+
+                                            $details[$i][$add_attribute_title] = $add_attribute_detail;
+                                        }
+                                    }
+                                    foreach ($titles as $title) {
+                                        echo '<tr>';
+                                        echo '<td>' . __($title) . '</td>';
+
+                                        for ($i = 0; $i < count($cookieArr); $i++) {
+                                            $detail = isset($details[$i][$title]) ? $details[$i][$title] : '-';
+                                            echo '<td>' . __($detail) . '</td>';
+                                        }
+                                        echo '</tr>';
+                                    }
+                                }
+                                ?>
+
+                                <!-- Standard features -->
+                                <?php if (!empty($rowcount)) : ?>
+                                    <tr>
+                                        <td class="gray-lt-bg-td" colspan="<?php echo $rowcount + 1; ?>"> <div class="inner-td-wrap"> <?php _e('Standard Features', 'btrailers-theme'); ?></div></td>
+                                    </tr>
+                                <?php endif;
+
+                                if (!empty($cookieArr)) {
+                                    echo '<tr>';
+                                    echo '<td>' . __('Standard Features', 'btrailers-theme') . '</td>';
+                                    foreach ($cookieArr as $trailerId) {
+                                        $features = get_field('add_standard_features', $trailerId);
+                                        $trailer_standard_features = array();
+                                        foreach ($features as $feature) {
+                                            array_push($trailer_standard_features, $feature['add_feature']);
+                                        }
+                                        echo '<td><ul>';
+                                        foreach ($trailer_standard_features as $feature) {
+                                            echo '<li>' . __($feature) . '</li>';
+                                        }
+                                        echo '</ul></td>';
                                     }
                                     echo '</tr>';
                                 }
-                            }
-                            ?>
+                                ?>
 
-                            <!-- Standard features -->
-                            <?php if (!empty($rowcount)) : ?>
-                                <tr>
-                                    <td class="gray-lt-bg-td" colspan="<?php echo $rowcount + 1; ?>"><?php _e('Standard Features', 'btrailers-theme'); ?></td>
-                                </tr>
-                            <?php endif;
+                                <!-- Aditional optional -->
+                                <?php if (!empty($rowcount)) : ?>
+                                    <tr>
+                                        <td class="gray-lt-bg-td" colspan="<?php echo $rowcount + 1; ?>"> <div class="inner-td-wrap">  <?php _e('Additional Options', 'btrailers-theme'); ?></div></td>
+                                    </tr>
+                                <?php endif;
 
-                            if (!empty($cookieArr)) {
-                                echo '<tr>';
-                                echo '<td>' . __('Standard Features', 'btrailers-theme') . '</td>';
-                                foreach ($cookieArr as $trailerId) {
-                                    $features = get_field('add_standard_features', $trailerId);
-                                    $trailer_standard_features = array();
-                                    foreach ($features as $feature) {
-                                        array_push($trailer_standard_features, $feature['add_feature']);
+                                if (!empty($cookieArr)) {
+                                    echo '<tr>';
+                                    echo '<td>' . __('Additional Options', 'btrailers-theme') . '</td>';
+                                    foreach ($cookieArr as $trailerId) {
+                                        $options = get_field('add_additional_options', $trailerId);
+                                        $trailer_additional_options = array();
+                                        foreach ($options as $option) {
+                                            array_push($trailer_additional_options, $option['add_options']);
+                                        }
+                                        echo '<td><ul>';
+                                        foreach ($trailer_additional_options as $option) {
+                                            echo '<li>' . __($option) . '</li>';
+                                        }
+                                        echo '</ul></td>';
                                     }
-                                    echo '<td><ul>';
-                                    foreach ($trailer_standard_features as $feature) {
-                                        echo '<li>' . __($feature) . '</li>';
-                                    }
-                                    echo '</ul></td>';
+                                    echo '</tr>';
                                 }
-                                echo '</tr>';
-                            }
-                            ?>
-
-                            <!-- Aditional optional -->
-                            <?php if (!empty($rowcount)) : ?>
-                                <tr>
-                                    <td class="gray-lt-bg-td" colspan="<?php echo $rowcount + 1; ?>"><?php _e('Additional Options', 'btrailers-theme'); ?></td>
-                                </tr>
-                            <?php endif;
-
-                            if (!empty($cookieArr)) {
-                                echo '<tr>';
-                                echo '<td>' . __('Additional Options', 'btrailers-theme') . '</td>';
-                                foreach ($cookieArr as $trailerId) {
-                                    $options = get_field('add_additional_options', $trailerId);
-                                    $trailer_additional_options = array();
-                                    foreach ($options as $option) {
-                                        array_push($trailer_additional_options, $option['add_options']);
-                                    }
-                                    echo '<td><ul>';
-                                    foreach ($trailer_additional_options as $option) {
-                                        echo '<li>' . __($option) . '</li>';
-                                    }
-                                    echo '</ul></td>';
-                                }
-                                echo '</tr>';
-                            }
-                            ?>
-                        </tbody>
-                    </table>
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
